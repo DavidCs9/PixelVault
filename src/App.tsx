30bb6fa2ac6c065843c07a9bd22a49bd716f9547
DavidCs9 <davidcastrosi@hotmail.com>
2025-05-08 18:19:55 +0000

    Update App component title and adjust layout spacing

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,10 @@ import GameList from "./components/GameList";
 
 function App() {
   return (
-    <div className="flex h-screen flex-col items-center gap-2 bg-gray-700 p-10">
+    <div className="flex h-screen flex-col items-center gap-3 bg-gray-700 p-10">
       <h1 className="flex flex-col items-center justify-center text-3xl font-bold text-white">
-        PixelVault
+        PixelVault - Retro Game Finder
       </h1>
-      <p className="text-white">
-        A collection of games from the past, present, and future.
-      </p>
       <GameList />
     </div>
   );
