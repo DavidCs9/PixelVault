b0331ff58629f7a2b008f5b51eff689c42ebeda5
DavidCs9 <davidcastrosi@hotmail.com>
2025-05-08 16:43:56 +0000

    add prettier with tailwind plugin

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
+    <div className="flex h-screen flex-col items-center justify-center bg-black">
+      <h1 className="flex flex-col items-center justify-center text-3xl font-bold text-white">
         Hello World from Pixel Vault
       </h1>
-    </>
+    </div>
   );
 }
 
