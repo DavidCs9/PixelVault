041aaf4c88641cbdf2da42d86017fd836052e11d
DavidCs9 <davidcastrosi@hotmail.com>
2025-05-08 19:25:19 +0000

    Add lucide-react dependency and update ThemeToggleButton to use icons for theme switching

--- src/components/ThemeToggleButton.tsx
+++ src/components/ThemeToggleButton.tsx
@@ -1,14 +1,15 @@
 import ThemeContext from "../context/ThemeContext";
+import { Moon, Sun } from "lucide-react";
 import { useContext } from "react";
 
 function ThemeToggleButton() {
   const { theme, toggleTheme } = useContext(ThemeContext);
   return (
     <button
-      className={`absolute top-0 right-0 cursor-pointer rounded-md p-2`}
+      className={`absolute top-3 right-3 cursor-pointer rounded-md p-2`}
       onClick={toggleTheme}
     >
-      {theme === "light" ? "Light Mode" : "Dark Mode"}
+      {theme === "light" ? <Sun /> : <Moon />}
     </button>
   );
 }
