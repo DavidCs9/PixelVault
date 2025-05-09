b3be22afa364c33b0d7e5923cc48e9554b7277e5
DavidCs9 <davidcastrosi@hotmail.com>
2025-05-09 22:30:38 +0000

    Optimize Wishlist route by implementing useMemo for sorting wishlist games by rating; enhance rendering logic to improve performance and user experience.

--- src/routes/wishlist.tsx
+++ src/routes/wishlist.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { useWishlist } from "../context/WishlistContext";
 import GameCard from "../components/GameCard";
+import { useMemo } from "react";
 
 export const Route = createFileRoute("/wishlist")({
   component: RouteComponent,
@@ -8,14 +9,18 @@ export const Route = createFileRoute("/wishlist")({
 
 function RouteComponent() {
   const { wishlist } = useWishlist();
+  const wishlistGames = useMemo(
+    () => Array.from(wishlist.values()).sort((a, b) => b.rating - a.rating),
+    [wishlist],
+  );
   return (
     <div className="flex flex-col gap-4">
       <h1 className="text-center text-2xl font-bold">Wishlist</h1>
-      {Array.from(wishlist).length === 0 ? (
+      {wishlistGames.length === 0 ? (
         <p className="text-center text-gray-500">No games in wishlist</p>
       ) : (
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-          {Array.from(wishlist.values()).map((game) => (
+          {wishlistGames.map((game) => (
             <GameCard key={game.id} game={game} />
           ))}
         </div>
