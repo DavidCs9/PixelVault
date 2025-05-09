eb1a3ab221b3f9c1697305877bad62e5850fbfae
DavidCs9 <davidcastrosi@hotmail.com>
2025-05-09 16:10:22 +0000

    Integrate theme context into Navbar for dynamic styling of wishlist count based on selected theme

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,10 @@ import ThemeToggleButton from "./ThemeToggleButton";
 import { Gamepad } from "lucide-react";
 import WishlistContext from "../context/WishlistContext";
 import { useContext } from "react";
+import ThemeContext from "../context/ThemeContext";
 
 function Navbar() {
+  const { theme } = useContext(ThemeContext);
   const { wishlist } = useContext(WishlistContext);
   return (
     <div className={`flex w-full items-center justify-between`}>
@@ -13,8 +15,12 @@ function Navbar() {
         <Gamepad className="h-8 w-8" /> PixelVault
       </h1>
       <div className={`flex items-center justify-center gap-2`}>
-        <div className={`text-sm`}>
-          Wishlist <span className={`text-sm font-bold`}>{wishlist.size}</span>
+        <div
+          className={`text-gray-300 ${
+            theme === "light" ? "text-gray-600" : "text-gray-300"
+          }`}
+        >
+          Wishlist <span className={`font-bold`}>{wishlist.size}</span>
         </div>
         <ThemeToggleButton />
       </div>
