667a5366753265eb0480a0cb5d278789dafe782e
DavidCs9 <davidcastrosi@hotmail.com>
2025-05-09 23:22:03 +0000

    Enhance GamesContext to dynamically set page size based on window width; implement useEffect for initial page size calculation and update fetchGames function to utilize the new page size.

--- src/context/GamesContext.tsx
+++ src/context/GamesContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 import { useQuery, keepPreviousData } from "@tanstack/react-query";
 import { GameSchema, type Game } from "../schemas/gameSchema";
 import useDebounce from "../hooks/useDebounce";
@@ -24,17 +24,28 @@ interface GamesProviderProps {
 
 export function GamesProvider({ children }: GamesProviderProps) {
   const [page, setPage] = useState(1);
+  const [pageSize, setPageSize] = useState<number | undefined>(undefined);
   const [search, setSearch] = useState("");
   const debouncedSearch = useDebounce(search, 500);
-  const PAGE_SIZE = 8;
   const FROM_DATE = "1980-01-01";
   const TO_DATE = "1999-12-31";
   const ORDERING = "-rating";
 
+  function calculatePageSize() {
+    if (window.innerWidth < 768) {
+      return 5;
+    }
+    return 8;
+  }
+
+  useEffect(() => {
+    setPageSize(calculatePageSize());
+  }, []);
+
   async function fetchGames(pageNumber: number) {
     const API_URL = `https://api.rawg.io/api/games?key=${
       import.meta.env.VITE_RAWG_API_KEY
-    }&dates=${FROM_DATE},${TO_DATE}&ordering=${ORDERING}&page_size=${PAGE_SIZE}&page=${pageNumber}&search=${debouncedSearch}`;
+    }&dates=${FROM_DATE},${TO_DATE}&ordering=${ORDERING}&page_size=${pageSize}&page=${pageNumber}&search=${debouncedSearch}`;
     const response = await fetch(API_URL);
     const rawData = await response.json();
     const cleanData = rawData.results.map((game: Game) => {
@@ -46,7 +57,7 @@ export function GamesProvider({ children }: GamesProviderProps) {
     const games = GameSchema.array().parse(cleanData);
     return {
       games,
-      totalPages: Math.ceil(rawData.count / PAGE_SIZE),
+      totalPages: Math.ceil(rawData.count / pageSize!),
     };
   }
 
@@ -55,6 +66,7 @@ export function GamesProvider({ children }: GamesProviderProps) {
     queryFn: () => fetchGames(page),
     placeholderData: keepPreviousData,
     staleTime: 1000 * 60 * 5,
+    enabled: !!pageSize,
   });
 
   const value = {
