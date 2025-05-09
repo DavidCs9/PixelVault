d8a3bbe38012bec2ad595e8a6fa24efe8f59d960
DavidCs9 <davidcastrosi@hotmail.com>
2025-05-09 19:23:47 +0000

    Implement Wishlist display in RouteComponent; utilize WishlistContext to render game cards dynamically within the wishlist route, enhancing user experience and navigation.

--- src/routes/wishlist.tsx
+++ src/routes/wishlist.tsx
@@ -1,9 +1,22 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { useContext } from "react";
+import WishlistContext from "../context/WishlistContext";
+import GameCard from "../components/GameCard";
 
 export const Route = createFileRoute("/wishlist")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/wishlist"!</div>;
+  const { wishlist } = useContext(WishlistContext);
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="text-center text-2xl font-bold">Wishlist</h1>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+        {Array.from(wishlist.values()).map((game) => (
+          <GameCard key={game.id} game={game} onClick={() => {}} />
+        ))}
+      </div>
+    </div>
+  );
 }
