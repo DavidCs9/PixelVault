882648b187b2501961b07b3c1e5541d3dbe2cc26
DavidCs9 <davidcastrosi@hotmail.com>
2025-05-09 18:05:56 +0000

    Enhance GameList component with pagination and improved API call; update fetchGames function to return total pages and integrate page navigation controls.

--- src/components/GameList.tsx
+++ src/components/GameList.tsx
@@ -1,18 +1,22 @@
 import { useState } from "react";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, keepPreviousData } from "@tanstack/react-query";
 import GameCard from "./GameCard";
 import GameDetailModal from "./GameDetailModal";
 import { GameSchema, type Game } from "../schemas/gameSchema";
+import { SkipBack, SkipForward } from "lucide-react";
 
 function GameList() {
   const [selectedGame, setSelectedGame] = useState<Game | null>(null);
-  const PAGE_SIZE = 12;
+  const [page, setPage] = useState(1);
+  const PAGE_SIZE = 8;
   const FROM_DATE = "1980-01-01";
   const TO_DATE = "1999-12-31";
   const ORDERING = "-rating";
 
-  async function fetchGames() {
-    const API_URL = `https://api.rawg.io/api/games?key=${import.meta.env.VITE_RAWG_API_KEY}&dates=${FROM_DATE},${TO_DATE}&ordering=${ORDERING}&page_size=${PAGE_SIZE}`;
+  async function fetchGames(pageNumber: number) {
+    const API_URL = `https://api.rawg.io/api/games?key=${
+      import.meta.env.VITE_RAWG_API_KEY
+    }&dates=${FROM_DATE},${TO_DATE}&ordering=${ORDERING}&page_size=${PAGE_SIZE}&page=${pageNumber}`;
     const response = await fetch(API_URL);
     const rawData = await response.json();
     const cleanData = rawData.results.map((game: Game) => {
@@ -22,16 +26,17 @@ function GameList() {
       };
     });
     const games = GameSchema.array().parse(cleanData);
-    return games;
+    return {
+      games,
+      totalPages: Math.ceil(rawData.count / PAGE_SIZE),
+    };
   }
 
-  const {
-    data: games,
-    isLoading,
-    error,
-  } = useQuery({
-    queryKey: ["games"],
-    queryFn: () => fetchGames(),
+  const { data, isLoading, error, isPlaceholderData } = useQuery({
+    queryKey: ["games", page], // Include page in the query key
+    queryFn: () => fetchGames(page),
+    placeholderData: keepPreviousData,
+    staleTime: 1000 * 60 * 5,
   });
 
   if (isLoading) {
@@ -52,15 +57,43 @@ function GameList() {
   }
 
   return (
-    <div className="container h-[80vh] overflow-auto">
+    <div className="container flex h-[80vh] flex-col gap-3 overflow-auto">
       <div className="grid h-full grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {games?.map((game) => (
+        {data?.games?.map((game) => (
           <GameCard
             key={game.id}
             game={game}
             onClick={() => setSelectedGame(game)}
           />
         ))}
+      </div>
+      <div className="flex justify-end gap-4">
+        <button
+          onClick={() => setPage((old) => Math.max(old - 1, 1))}
+          disabled={page === 1}
+        >
+          <SkipBack />
+        </button>
+        <span className="flex items-center">
+          Page {page} of {data?.totalPages || "?"}
+        </span>
+        <button
+          onClick={() => {
+            if (
+              !isPlaceholderData &&
+              data?.totalPages &&
+              page < data.totalPages
+            ) {
+              setPage((old) => old + 1);
+            }
+          }}
+          disabled={
+            isPlaceholderData ||
+            (data?.totalPages ? page >= data.totalPages : false)
+          }
+        >
+          <SkipForward />
+        </button>
       </div>
       {selectedGame && (
         <GameDetailModal
